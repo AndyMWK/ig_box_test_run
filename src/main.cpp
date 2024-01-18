9910a678cc9eeead713773c1afb968337d1fe4c2
Andy Kim <mwandy2003@gmail.com>
2024-01-18 09:08:18 -0800

    jan 18th save

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 #include <Arduino.h>
-#include "st7735.h"
-#include "st7735_reg.h"
 #include "LoRa.h"
 #include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_ST7735.h"
 
 //Definition of constants
-#define LED_PIN PA11
+#define LED_PIN PC13
 #define LoRa_ID 915E6
+
+//these pins will change
+#define TFT_CS A8
+#define TFT_RST A9
+#define TFT_A0 A1
 
 //Definition of contant variables
 const int tries_before_quit = 10;
 const bool transmitter = true;
 
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_A0, TFT_RST);
+
 //create display object
-ST7735_Object_t display;
 
 //function declarations
 void reset();
+void tftPrintTest();
 
 // set up function: 
 // - set up pins
@@ -29,41 +36,35 @@ void reset();
 void setup() {
 
   //Pin initialization
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   //serial monitor for debugging. CHECK BAUD RATE
-  Serial.begin(9600);
+  tft.initR(INITR_BLACKTAB);
+  tft.setTextColor(ST7735_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.print("Hello, STM32 Blue Pill!");
   
   //LoRa initalization
-  if(!LoRa.begin(LoRa_ID)) {
-    Serial.println("LoRa initialization failed!");
-    int iterations = 0;
+  // if(!LoRa.begin(LoRa_ID)) {
+  //   Serial.println("LoRa initialization failed!");
+  //   int iterations = 0;
 
-    while(iterations < tries_before_quit) {
-      if(LoRa.begin(LoRa_ID)) {
-        iterations = tries_before_quit;
-      } else {
-        iterations++;
-      }
-    }
-  }
+  //   while(iterations < tries_before_quit) {
+  //     if(LoRa.begin(LoRa_ID)) {
+  //       iterations = tries_before_quit;
+  //     } else {
+  //       iterations++;
+  //     }
+  //   }
+  // }
 
-  //Display initialization
-  if(ST7735_Init(&display, ST7735_FORMAT_RBG565, ST7735_ORIENTATION_LANDSCAPE) == ST7735_ERROR) {
-    Serial.println("Display initialization failed!");
-    int i = 0;
-    while(i < tries_before_quit) {
-      if(!ST7735_Init(&display, ST7735_FORMAT_RBG565, ST7735_ORIENTATION_LANDSCAPE) == ST7735_ERROR) {
-        i = tries_before_quit;
-      } else {
-        i++;
-      }
-    }
-  }
+
   //validation state - validates or timeout
 }
 
 bool send_state = false;
+
 void loop() {
   //use state machines
   
@@ -72,21 +73,60 @@ void loop() {
   //armed state - lasts for as long as it needs to
 
   //send state - send signal and transition to ground state
-  if(send_state) {
-    LoRa.beginPacket();
-    LoRa.print(1);
-    LoRa.endPacket();
-    reset();
-  }
-  
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 
   
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  // if(send_state) {
+  //   LoRa.beginPacket();
+  //   LoRa.print(1);
+  //   LoRa.endPacket();
+  //   reset();
+  // }
 }
 
 void reset() {
   send_state = false;
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST7735_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST7735_GREEN);
+  tft.print(6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST7735_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST7735_WHITE);
+  tft.print(" seconds.");
 }
