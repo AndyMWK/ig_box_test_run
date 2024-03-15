c4a8e1d32ede792d583c12697db25bf68e91641a
Andy Kim <mwandy2003@gmail.com>
2024-03-15 00:18:05 -0700

    march15th save

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@
 #define TFT_RST PA9
 #define TFT_A0 PA10
 
+#define ARMING_KEY PB9
+#define E_MATCH PC15
+
 //Definition of contant variables
 const bool sender = false;
 
@@ -41,6 +44,9 @@ void reset();
 //   the object by this amount)
 
 void printText(uint16_t padding, Adafruit_ST7735 &display, char msg[]);
+void ISR_pb11();
+
+bool interrupt_flag = false;
 
 void setup() {
 
@@ -48,7 +54,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   //serial monitor for debugging. CHECK BAUD RATE
-  if(!sender) {
+  if(sender) {
     tft.initR(INITR_BLACKTAB);
 
     tft.fillScreen(ST7735_WHITE);
@@ -81,24 +87,21 @@ void setup() {
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);	
     delay(500);									
-	} 
+	}
+
+  if(!sender) {
+    pinMode(E_MATCH, OUTPUT);
+  } 
+
+  //interupt routines
+  attachInterrupt(ARMING_KEY, ISR_pb11, HIGH);
 }
 
 bool send_state = false;
+bool arming_signal_received = false;
+bool send_LoRa(char string[]);
 
 void loop() {
-  //use state machines
-  
-  //ground state - looks for armed state activation
-
-  //armed state - lasts for as long as it needs to
-
-  //send state - send signal and transition to ground state
-
-  // digitalWrite(LED_BUILTIN, HIGH);
-  // delay(500);
-  // digitalWrite(LED_BUILTIN, LOW);
-  // delay(500);
 
   if(!sender) {
     int packet_size = LoRa.parsePacket();
@@ -112,14 +115,53 @@ void loop() {
       tft.drawRGBBitmap(35, 10, ubcRocket, UBCROCKET_WIDTH, UBCROCKET_HEIGHT);
     }
     delay(1000);
+
+    //get arming on the receiver end - look for interrupt flag
+
+    //send to the sender 
+
+    //then look for receiving signal from the sender. 
+
+    //if input signal is received, after a little bit of delay, 
+
+     if (interrupt_flag) {
+      if(send_LoRa("armed")) {
+        int packet_size = LoRa.parsePacket();
+
+        if(packet_size) {
+          //anlayze and then send e-match signal. 
+        }
+      }
+    }
   }
 
   else if(sender) {
-    LoRa.beginPacket();
-    LoRa.print("send");
-    LoRa.endPacket();
+    int packet_size = LoRa.parsePacket();
 
-    delay(100);
+    if(packet_size) {
+      //read packet
+
+      arming_signal_received = true;
+    }
+
+    if (arming_signal_received) {
+
+      delay(500);
+
+      if (interrupt_flag) {
+        LoRa.beginPacket();
+        LoRa.print("send");
+        LoRa.endPacket();
+
+        delay(100);
+      }
+      else if(!interrupt_flag) {
+        //display basic screen instructions for arming and sending
+      }
+    }
+    else {
+      //display something
+    }
   }
 }
 
@@ -127,6 +169,33 @@ void reset() {
   send_state = false;
 }
 
+void ISR_pb11() {
+  if(sender && arming_signal_received) {
+    interrupt_flag = true;
+  }
+  else if(!sender) {
+    interrupt_flag = true;
+  }
+}
+
+bool send_LoRa(char string[]) {
+  int count = 0;
+  while(count < 10) {
+    LoRa.beginPacket();
+    LoRa.print(string);
+    LoRa.endPacket();
+
+    delay(100);
+
+    count++;
+  }
+
+  return true;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+//printing text. Can use for error messages
 void printText(uint16_t padding, Adafruit_ST7735 &display, char msg[]) {
 
   display.setCursor(padding, padding);
@@ -155,5 +224,4 @@ void printText(uint16_t padding, Adafruit_ST7735 &display, char msg[]) {
     word[word_iterator] = msg[i];
     word_iterator++;
   }
-
 }
